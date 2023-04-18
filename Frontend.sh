ad21abc26db07f91cb0ac560a09c86ea5c23bf0b
kcvamsi <kcvamsi98@gmail.com>
2023-04-18 20:22:29 +0530

    cp

--- Frontend.sh
+++ Frontend.sh
@@ -4,6 +4,7 @@ rm -rf /usr/share/nginx/html/*
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip 
 cd /usr/share/nginx/html 
 unzip /tmp/frontend.zip 
+cp frontend.conf /etc/nginx/default.d/roboshop.conf
 
 # here we need to add a config file. we cannt add directly the cmnd here, so 1st we need to create a config file seperately and then copy that file to the current location
 
