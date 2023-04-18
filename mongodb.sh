381521a892962adeffab9961a40d92992509db09
kcvamsi <kcvamsi98@gmail.com>
2023-04-18 20:32:20 +0530

    mongod

--- mongodb.sh
+++ mongodb.sh
@@ -2,8 +2,9 @@
 
 cp mongodb.repo /etc/yum.repos.d/mongo.repo
 yum install mongodb-org -y 
+sed -i -e 's|127.0.0.1|0.0.0.0|' /etc/mongod.conf
 systemctl enable mongod 
-systemctl start mongod 
+systemctl restart mongod 
 
 ## here we nend to change the ip from 127.0.0.0 to 0.0.0.0
 but the editing of file cant be done here.
