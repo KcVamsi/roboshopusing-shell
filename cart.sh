a3a0d668445f688a3210e8de596b1f3dc2528179
kcvamsi <kcvamsi98@gmail.com>
2023-04-18 20:37:28 +0530

    cat

--- cart.sh
+++ cart.sh
@@ -6,7 +6,10 @@ curl -L -o /tmp/cart.zip https://roboshop-artifacts.s3.amazonaws.com/cart.zip
 cd /app 
 unzip /tmp/cart.zip
 npm install 
-cp cart.service vim /etc/systemd/system/cart.service
+cp cart.service /etc/systemd/system/cart.service
 systemctl daemon-reload
 systemctl enable cart 
 systemctl start cart
+cp mongo.repo /etc/yum.repos.d/mongo.repo
+yum install mongodb-org-shell -y
+mongo --host mongodb.devopsdomain1.online </app/schema/catalogue.js
