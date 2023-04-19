eba3d1f414e97f247dab484091b76e5b59eb7571
kcvamsi <kcvamsi98@gmail.com>
2023-04-19 13:52:24 +0530

    sp

--- shipping.sh
+++ shipping.sh
@@ -1,15 +1,25 @@
+echo -e "\e[31minstall maven\e[0m"
 yum install maven -y
+echo -e "\e[31muser add\e[0m"
 useradd roboshop
+echo -e "\e[31mmake app dir\e[0m"
+rm -rf /app
 mkdir /app 
+echo -e "\e[31mdownloading maven\e[0m"
 curl -L -o /tmp/shipping.zip https://roboshop-artifacts.s3.amazonaws.com/shipping.zip 
 cd /app 
+echo -e "\e[31munzipping\e[0m"
 unzip /tmp/shipping.zip
 mvn clean package 
 mv target/shipping-1.0.jar shipping.jar 
-cp shipping.service vim /etc/systemd/system/shipping.service
+echo -e "\e[31mdcopying files\e[0m"
+cp /home/centos/roboshopusing-shell/shipping.service vim /etc/systemd/system/shipping.service
+echo -e "\e[31mstarting the service\e[0m"
 systemctl daemon-reload
 systemctl enable shipping 
 systemctl start shipping
+echo -e "\e[31mdownloading the repo\e[0m"
 yum install mysql -y 
+echo -e "\e[31mpassword\e[0m"
 mysql -h mysql.devopsdomain1.online -uroot -pRoboShop@1 < /app/schema/shipping.sql 
 systemctl restart shipping
