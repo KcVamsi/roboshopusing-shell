252749a82aa92f348a672a2968d9ed16d155806c
kcvamsi <kcvamsi98@gmail.com>
2023-04-19 13:35:05 +0530

    mq

--- mysql.sh
+++ mysql.sh
@@ -1,7 +1,12 @@
+echo -e "\e[31mdisbale\e[0m"
 dnf module disable mysql -y 
-cp mysql.repo vim /etc/yum.repos.d/mysql.repo
+echo -e "\e[31mcopy file\e[0m"
+cp /home/centos/roboshopsuing-shell/mysql.repo vim /etc/yum.repos.d/mysql.repo
+echo -e "\e[31minstall\e[0m"
 yum install mysql-community-server -y
+echo -e "\e[31mstarting the service\e[0m"
 systemctl enable mysqld
-systemctl start mysqld  
+systemctl restart mysqld  
+echo -e "\e[31mmysql password\e[0m"
 mysql_secure_installation --set-root-pass RoboShop@1
 mysql -uroot -pRoboShop@1
