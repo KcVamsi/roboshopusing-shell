ec68210c913f1a120ee89ce29cbf1ce0ced4b184
kcvamsi <kcvamsi98@gmail.com>
2023-04-19 12:14:06 +0530

    front

--- Frontend.sh
+++ Frontend.sh
@@ -1,10 +1,9 @@
 yum install nginx -y 
-cp frontend.config  /etc/nginx/default.d/roboshop.conf 
+cp frontend.config /etc/nginx/default.d/roboshop.conf 
 rm -rf /usr/share/nginx/html/* 
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip 
 cd /usr/share/nginx/html 
-unzip /tmp/frontend.zip 
-cp frontend.conf /etc/nginx/default.d/roboshop.conf
+unzip /tmp/frontend.zip `
 
 # here we need to add a config file. we cannt add directly the cmnd here, so 1st we need to create a config file seperately and then copy that file to the current location
 
