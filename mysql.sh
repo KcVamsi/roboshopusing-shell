ee7d99aea73052eb55ef5882c4f5ad622dd1ee2f
kcvamsi <kcvamsi98@gmail.com>
2023-04-19 13:41:48 +0530

    cp

--- mysql.sh
+++ mysql.sh
@@ -1,7 +1,7 @@
 echo -e "\e[31mdisbale\e[0m"
 dnf module disable mysql -y 
 echo -e "\e[31mcopy file\e[0m"
-cp /home/centos/roboshopsuing-shell/mysql.repo /etc/yum.repos.d/mysql.repo
+cp /home/centos/roboshopusing-shell/mysql.repo /etc/yum.repos.d/mysql.repo
 echo -e "\e[31minstall\e[0m"
 yum install mysql-community-server -y
 echo -e "\e[31mstarting the service\e[0m"
