aa3560d770cea98d4e9d7a8a9f0a091a05110cbb
kcvamsi <kcvamsi98@gmail.com>
2023-04-21 15:17:41 +0530

    inpu f

--- common.sh
+++ common.sh
@@ -1,26 +1,30 @@
 app_user=roboshop
 
+print_head(){
+    echo -e "\e[31m$1\e[0m"
+}
+
 function_nodejs(){
 
-echo -e "\e[31mConfigure the application\e[0m"
+print_head "Configure the application"
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash
-echo -e "\e[31minstalling the application\e[0m"
+print_head "installing the application
 yum install nodejs -y
-echo -e "\e[31madding the user\e[0m"
+print_head "adding the user"
 useradd ${app_user}
-echo -e "\e[31mCreating the directory\e[0m"
+print_head "Creating the directory"
 rm -rf /app
 mkdir /app 
-echo -e "\e[31mdownloading the application\e[0m"
+print_head "downloading the application"
 curl -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip 
-echo -e "\e[31mChanging the directory\e[0m"
+print_head "Changing the directory"
 cd /app 
-echo -e "\e[31munzipping the application\e[0m"
+print_head "unzipping the application"
 unzip /tmp/${component}.zip 
 npm install 
-echo -e "\e[31mCopying the files\e[0m"
+print_head "Copying the files"
 cp ${script_path}/${component}.service /etc/systemd/system/${component}.service
-echo -e "\e[31mstarting the application\e[0m"
+print_head "starting the application"
 systemctl daemon-reload
 systemctl enable ${component} 
 systemctl restart ${component}
---
+++ functions.sh
@@ -0,0 +1,32 @@
+#how to declare a function
+
+function_name(){
+
+echo hello world
+
+}
+
+
+#how to call a function = just give the name - function_name
+
+
+# inputs to the function
+
+# we can send inputs to the function and can access them by using . $1, $#, $*, $n
+
+#echo first argument - $1
+
+#echo second argument - $2
+
+#echo all argument - $*
+
+#echo number of argument - $#
+
+#print_head is the name of the function(){}
+
+#in this we give the command
+
+# $1 is used if we give only one input " all the lines in the double quotes are considered as one input"
+#for mulitple inputs we use $*
+
+
