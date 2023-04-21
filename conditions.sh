4ecf70119cd63fbada67e683563eb72a8a4fe1fa
kcvamsi <kcvamsi98@gmail.com>
2023-04-21 20:05:08 +0530

    if else

--- conditions.sh
+++ conditions.sh
@@ -1,10 +1,34 @@
 # if, if else, else if.
-fruit_name=mango
 
+# if example 
+
+fruit_name=mango
+quantity=10
+
+# keep the syntax in mind because of the spaces and the "" we are getting error. and also fi in the last
+
+if [ "$fruit_name" == "mango" ];
+
+then
+
+echo mango quantity - $quantity
+
+fi
+
+
+#if else example
+
+fruit_name=mango
 quantity=10
 
 if [ "$fruit_name" == "mango" ];
+
 then
+
 echo mango quantity - $quantity
+
+else
+
+echo fruit not exist
 
 fi
