4bc31201326d6e3ea3bc3d20aae4fe2433db4234
kcvamsi <kcvamsi98@gmail.com>
2023-04-21 13:44:46 +0530

    cp

--- shipping.sh
+++ shipping.sh
@@ -16,12 +16,12 @@ echo -e "\e[31munzipping\e[0m"
 unzip /tmp/shipping.zip
 mvn clean package 
 mv target/shipping-1.0.jar shipping.jar 
+echo -e "\e[31mcopying files\e[0m"
+cp ${script_path}/shipping.service /etc/systemd/system/shipping.service
 echo -e "\e[31mdownloading the repo\e[0m"
 yum install mysql -y 
 echo -e "\e[31mpassword\e[0m"
 mysql -h mysql.devopsdomain1.online -uroot -p${mysql_root_password} < /app/schema/shipping.sql 
-echo -e "\e[31mcopying files\e[0m"
-cp ${script_path}/shipping.service /etc/systemd/system/shipping.service
 echo -e "\e[31mstarting the service\e[0m"
 systemctl daemon-reload
 systemctl enable shipping
