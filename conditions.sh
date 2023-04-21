75af4a590edeb7e30a709a51701bbab793a4a826
kcvamsi <kcvamsi98@gmail.com>
2023-04-21 20:07:31 +0530

    argug

--- conditions.sh
+++ conditions.sh
@@ -32,3 +32,21 @@ else
 echo fruit not exist
 
 fi
+
+# now instead of using directly the mango we can use any name but for that we need use $1 and $2 ad they are arguments
+# now we need to provide the inputs to the function
+
+fruit_name=$1
+quantity=$2
+
+if [ "$fruit_name" == "mango" ];
+
+then
+
+echo mango quantity - $quantity
+
+else
+
+echo fruit not exist
+
+fi
