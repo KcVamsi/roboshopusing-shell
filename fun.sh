95f5b174569262835d2329097e5dc296ca462084
kcvamsi <kcvamsi98@gmail.com>
2023-04-22 17:55:39 +0530

    fu

---
+++ fun.sh
@@ -0,0 +1,11 @@
+ function_name(){
+
+  echo hello
+  return 1
+  # 1 is the xit status and ranges from 0 -255
+
+ }
+
+ # we have declared the function now we need to call it, forthat just give name
+
+ function_name
