8c7b6a4c96b7a402008b740d977f708b32997ef0
kcvamsi <kcvamsi98@gmail.com>
2023-04-22 13:58:45 +0530

    dis

--- dispatch.sh
+++ dispatch.sh
@@ -7,19 +7,20 @@ echo input missing
 exit
 fi
 source ${script_path}/common.sh
-echo -e "\e[31minstalling package\e[0m"
+echo -e "\e[31minstalling golang\e[0m"
 yum install golang -y
-echo -e "\e[31minstalling package\e[0m"
+echo -e "\e[31madding app\e[0m"
 useradd ${app_user}
-echo -e "\e[31minstalling package\e[0m"
+echo -e "\e[31changing directory\e[0m"
 rm -rf /app
 mkdir /app 
-echo -e "\e[31minstalling package\e[0m"
+echo -e "\e[31mdownloading package\e[0m"
 curl -L -o /tmp/dispatch.zip https://roboshop-artifacts.s3.amazonaws.com/dispatch.zip 
 cd /app 
-echo -e "\e[31minstalling package\e[0m"
+echo -e "\e[31munzipping package\e[0m"
 unzip /tmp/dispatch.zip
 sed -i -e "s|rabbitmq_app_password|${rabbitmq_app_password}|" ${script_path}/dispatch.service
+cp ${script_path}/dispatch.service /etc/systemd/system/dispatch.service
 go mod init dispatch
 go get 
 go build
